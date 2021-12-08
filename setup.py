6aebe286997931a70bb50c98dceada1092293fee
Jacob Renn <jacob.renn@squared.ai>
2021-12-08 08:15:57 -0500

    moving requirements to setup

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 from setuptools import setup
 from aisquared import __version__
 
-with open('requirements.txt', 'r') as f:
-    install_requires = [line for line in f.read().splitlines() if len(line) > 0]
-
 setup(
     name = 'aisquared',
     version = __version__,
@@ -14,5 +11,7 @@ setup(
     descritption = 'Utilities for interacting with the AI Squared Technology Stack',
     long_description = open('README.md').read(),
     long_description_content_type = 'text/markdown',
-    install_requires = install_requires
+    install_requires = [
+        'tensorflowjs'
+    ]
 )
