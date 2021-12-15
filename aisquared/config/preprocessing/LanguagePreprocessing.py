a3100d2ce50ab8b0406c8aea89c144a1dc183e0c
Jacob Renn <jacob.renn@squared.ai>
2021-12-15 14:45:57 -0500

    refactoring LanguagePreprocessor to support steps

--- aisquared/config/preprocessing/LanguagePreprocessing.py
+++ aisquared/config/preprocessing/LanguagePreprocessing.py
@@ -1,4 +1,12 @@
 import json
+from .Steps import Tokenize, RemoveCharacters, ConvertToCase, ConvertToVocabulary, PadSequences
+
+ALLOWED_STEPS = (
+    Tokenize,
+    RemoveCharacters,
+    ConvertToCase,
+    ConvertToVocabulary
+)
 
 class LanguagePreprocessor:
     """
@@ -6,48 +14,30 @@ class LanguagePreprocessor:
     """
     def __init__(
             self,
-            vocabulary,
-            max_vocab = None,
-            strip_punctuation = True,
-            lowercase = True,
-            padding_character = 0,
-            start_character = 1,
-            oov_character = 2,
-            padding = 'post',
-            max_len = 128
+            steps = None
     ):
         """
         Parameters
         ----------
-        vocabulary : dict
-            Dictionary of string to int mappings for vocabulary
-        max_vocab : int or None (default None)
-            The maximum vocabulary character to use. If None, all words are used
-        strip_punctuation : bool (default True)
-            Whether punctuation should be stripped in input text
-        lowercase : bool (default True)
-            Whether text should be lowercased in input text
-        padding_character : int (default 0)
-            Character to indicate padding
-        start_character : int (default 1)
-            Character for the start of sequences
-        oov_character : int (default 2)
-            Character to indicate out of vocabulary words
-        padding : str (default 'post')
-            Whether padding should occur at the beginning or the end of sequences
-        max_len : int (default 128)
-            Maximum length of sequences
+        steps : list or None (default None)
+            List of preprocessing steps for natural language
         """
 
-        self.vocabulary = vocabulary
-        self.max_vocab = max_vocab
-        self.strip_punctuation = strip_punctuation
-        self.lowercase = lowercase
-        self.padding_character = padding_character
-        self.start_character = start_character
-        self.oov_character = oov_character
-        self.padding = padding
-        self.max_len = max_len
+        self.steps = None
+        if steps is not None:
+            for step in steps:
+                self.add_step(step)
+
+    def add_step(self, step):
+        """
+        Add a step to the preprocessor object
+        """
+        if not isinstance(step, ALLOWED_STEPS):
+            raise TypeError(f'Each step must be one of {ALLOWED_STEPS}')
+        if self.steps is None:
+            self.steps = [step]
+        else:
+            self.steps = self.steps + [step]
 
     def to_dict(self):
         """
@@ -55,17 +45,9 @@ class LanguagePreprocessor:
         """
         return {
             'className' : 'LanguagePreprocessor',
-            'params' : {
-                'vocabulary' : self.vocabulary,
-                'maxVocab' : self.max_vocab,
-                'stripPunctuation' : self.strip_punctuation,
-                'lowercase' : self.lowercase,
-                'paddingCharacter' : self.padding_character,
-                'startCharacter' : self.start_character,
-                'oovCharacter' : self.oov_character,
-                'padding' : self.padding,
-                'maxLen' : self.max_len
-            }
+            'steps' : [
+                step.to_dict() for step in self.steps
+            ]
         }
 
     def to_json(self):
