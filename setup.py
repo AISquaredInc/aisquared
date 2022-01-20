50afe177a6ab4997ba6f9e0597d3c5b4d0628f59
Jacob Renn <jacob.renn@squared.ai>
2022-01-20 10:18:49 -0500

    updating email

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     url = 'https://github.com/AISquaredInc/aisquared',
     packages = ['aisquared', 'aisquared.config', 'aisquared.config.preprocessing', 'aisquared.config.postprocessing'],
     author = 'The AI Squared Team',
-    author_email = 'mann@squared.ai',
+    author_email = 'staff@squared.ai',
     descritption = 'Utilities for interacting with the AI Squared Technology Stack',
     long_description = open('README.md').read(),
     long_description_content_type = 'text/markdown',
