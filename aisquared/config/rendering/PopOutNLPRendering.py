215182cf656ee07f7e6a0d7b22b07a46c41421d5
Jacob Renn <jacob.renn@squared.ai>
2022-01-26 09:16:56 -0500

    adding include_prediction and include_score

--- aisquared/config/rendering/PopOutNLPRendering.py
+++ aisquared/config/rendering/PopOutNLPRendering.py
@@ -1,16 +1,23 @@
 import json
+from xml.etree.ElementInclude import include
 
 class PopOutNLPRendering:
     def __init__(
         self,
         document_list
+        include_prediction = True,
+        include_score = True,
     ):
         self.document_list = document_list
+        self.include_prediction = include_prediction
+        self.include_score = include_score
 
     def to_dict(self):
         return {
             'className' : 'PopOutNLPRendering',
             'params' : {
+                'includePrediction' : self.include_prediction,
+                'includeScore' : self.include_score,
                 'documentList' : self.document_list
             }
         }
