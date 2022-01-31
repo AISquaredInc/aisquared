a9cfe1e48703d3b7ef3506acc17a0be127097396
Jacob Renn <jacob.renn@squared.ai>
2022-01-31 09:55:20 -0500

    removing keywords and files

--- aisquared/config/postprocessing/MulticlassClassification.py
+++ aisquared/config/postprocessing/MulticlassClassification.py
@@ -15,15 +15,9 @@ class MulticlassClassification(BaseObject):
         ----------
         label_map : list
             A list of values to map the output of the model to
-        keywords : list of list of strings or None (default None)
-            List of list of keywords corresponding to the top keywords for every class
-        documents : list of list of strings or None (default None)
-            List of list of document URLs corresponding to the top documents for every class
         """
         super().__init__()
         self.label_map = label_map
-        self.keywords = keywords
-        self.documents = documents
 
     @property
     def label_map(self):
@@ -36,37 +30,11 @@ class MulticlassClassification(BaseObject):
             raise ValueError('For multiclass classification, the label map must have more than two values. If there are only two values, use the `BinaryClassification` class')
         self._label_map = value
 
-    @property
-    def keywords(self):
-        return self._keywords
-    @keywords.setter
-    def keywords(self, value):
-        if value is not None:
-            if not isinstance(value, list) or not all([isinstance(val, list) for val in value]) or not all([isinstance(v, str) for val in value for v in val]):
-                raise TypeError('keywords should be list of list of str')
-            if len(value) != len(self.label_map):
-                raise ValueError('Length of keywords should be the same as number of classes')
-        self._keywords = value
-
-    @property
-    def documents(self):
-        return self._documents
-    @documents.setter
-    def documents(self, value):
-        if value is not None:
-            if not isinstance(value, list) or not all([isinstance(val, list) for val in value]) or not all([isinstance(v, str) for val in value for v in val]):
-                raise TypeError('documents should be list of list of str')
-            if len(value) != len(self.label_map):
-                raise ValueError('Length of documents should be the same as number of classes')
-        self._documents = value
-        
     def to_dict(self):
         return {
             'className' : 'MulticlassClassification',
             'params' : {
-                'labelMap' : self.label_map,
-                'keywords' : self.keywords,
-                'documents' : self.documents
+                'labelMap' : self.label_map
             }
         }
         
