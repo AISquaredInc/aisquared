90bd35f04c423aa568ab83423f8c64c880a38f85
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2022-04-29 09:06:47 -0400

    moving things around and building out serving subdirectory

---
+++ aisquared/serving/__init__.py
@@ -0,0 +1,5 @@
+from mlflow.tensorflow import save_model as save_tensorflow_model
+from mlflow.sklearn import save_model as save_sklearn_model
+from mlflow.pytorch import save_model as save_pytorch_model
+from mlflow.keras import save_model as save_keras_model
+
---
+++ aisquared/serving/deploy.py
@@ -0,0 +1,9 @@
+from flask import Flask, request
+
+def deploy_model(
+        saved_model,
+        model_type,
+        host = 'localhost',
+        port = '2244'
+):
+    pass
