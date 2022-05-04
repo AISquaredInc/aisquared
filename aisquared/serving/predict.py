66c45ffe8e1b86589ed03b737af5aabc14c2a572
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2022-05-03 21:04:02 -0400

    adding predict function

---
+++ aisquared/serving/predict.py
@@ -0,0 +1,69 @@
+import numpy as np
+import requests
+import json
+
+def predict(
+    data,
+    host = 'http://127.0.0.1',
+    port = 2244
+):
+    """
+    Send data to use for prediction
+
+    Parameters
+    ----------
+    data : dict, str, np.ndarray, or list
+        The data to be predicted on
+    host : str (default 'http://127.0.0.1')
+        The host to use
+    port : int (default '2244')
+        The port to use
+    
+    Notes
+    -----
+    - If data is a dictionary, it is expected to already be 
+      correctly formatted
+    - If data is a string, it is expected to already be 
+      correctly formatted
+
+    Returns
+    -------
+    predictions : list
+        The predictions from the deployed model
+    """
+    # Setup the url and headers
+    url = f'{host}:{port}/predict'
+    headers = {
+        'Content-Type' : 'application/json'
+    }
+
+    # Format the data
+    if isinstance(data, dict):
+        data = json.dumps(data)
+    elif isinstance(data, str):
+        data = data
+    elif isinstance(data, np.ndarray):
+        data = json.dumps(
+            {
+                'data' : data.tolist()
+            }
+        )
+    elif isinstance(data, list):
+        data = json.dumps(
+            {
+                'data' : data
+            }
+        )
+    
+    # Make the request
+    with requests.session() as sess:
+        resp = sess.post(
+            url,
+            data = data,
+            headers = headers
+        )
+    
+    if resp.status_code != 200:
+        raise ValueError(f'Something went wrong. Status code {resp.status_code}')
+    else:
+        return resp.json()['predictions']
