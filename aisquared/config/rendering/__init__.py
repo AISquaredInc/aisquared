a16e6f68e18ce02cef472002b8e97f67a32d22b8
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2022-06-02 11:47:20 -0400

    importing new object

--- aisquared/config/rendering/__init__.py
+++ aisquared/config/rendering/__init__.py
@@ -6,4 +6,4 @@ from .ImageRendering import ImageRendering
 from .ObjectRendering import ObjectRendering
 from .DocumentRendering import DocumentRendering
 from .WordRendering import WordRendering
-from .ChainRendering import ChainRendering
+from .FilterRendering import FilterRendering
