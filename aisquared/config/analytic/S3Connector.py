2d6f17b0ce84660a5ba2d907de864a795275ad81
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2022-07-05 13:04:09 -0400

    running autopep8

--- aisquared/config/analytic/S3Connector.py
+++ aisquared/config/analytic/S3Connector.py
@@ -1,9 +1,11 @@
 from aisquared.base import BaseObject
 
+
 class S3Connector(BaseObject):
     """
     Interaction with an analytic stored in S3
     """
+
     def __init__(
         self,
         bucket,
@@ -16,7 +18,7 @@ class S3Connector(BaseObject):
             The bucket the file lives in
         key : str
             The key for the specific file
-        """    
+        """
         super().__init__()
         self.bucket = bucket
         self.key = key
@@ -24,6 +26,7 @@ class S3Connector(BaseObject):
     @property
     def bucket(self):
         return self._bucket
+
     @bucket.setter
     def bucket(self, value):
         if not isinstance(value, str):
@@ -33,6 +36,7 @@ class S3Connector(BaseObject):
     @property
     def key(self):
         return self._key
+
     @key.setter
     def key(self, value):
         if not isinstance(value, str):
@@ -44,9 +48,9 @@ class S3Connector(BaseObject):
         Get the configuration object as a dictionary
         """
         return {
-            'className' : 'S3Connector',
-            'params' : {
-                'bucket' : self.bucket,
-                'key' : self.key
+            'className': 'S3Connector',
+            'params': {
+                'bucket': self.bucket,
+                'key': self.key
             }
         }
