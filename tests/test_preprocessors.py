ebe736d69fd9e81088fad23b51b6a2afa4d91c15
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2022-07-06 14:12:09 -0400

    adding preprocessor tests

--- tests/test_preprocessors.py
+++ tests/test_preprocessors.py
@@ -26,3 +26,29 @@ def test_steps_init():
     aisquared.config.preprocessing.tabular.MinMax([0, 0, 0], [1, 1, 1])
     aisquared.config.preprocessing.tabular.OneHot(2, ['cat', 'dog', 'lizard'])
     aisquared.config.preprocessing.tabular.DropColumn(4)
+
+def test_processers_init():
+    tabular = aisquared.config.preprocessing.tabular.TabularPreprocessor()
+    image = aisquared.config.preprocessing.image.ImagePreprocessor()
+    text = aisquared.config.preprocessing.text.TextPreprocessor()
+    
+    tabular.add_step(
+        aisquared.config.preprocessing.tabular.ZScore([0], [0])
+    )
+    image.add_step(
+        aisquared.config.preprocessing.image.SubtractValue(10)
+    )
+    text.add_step(
+        aisquared.config.preprocessing.text.RemoveCharacters()
+    )
+
+    with pytest.raises(TypeError):
+        tabular.add_step(
+            aisquared.config.preprocessing.text.RemoveCharacters()
+        )
+        image.add_step(
+            aisquared.config.preprocessing.tabular.ZScore([0], [0])
+        )
+        text.add_step(
+            aisquared.config.preprocessing.image.SubtractValue(10)
+        )
