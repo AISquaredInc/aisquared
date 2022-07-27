24376c432431a162d1b03a240ddf6e8e623bf1b7
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2022-07-27 08:10:55 -0400

    updating local onnx model
    Signed-off-by: Jacob Renn <77127228+jacobrenn@users.noreply.github.com>

--- aisquared/config/analytic/LocalONNXModel.py
+++ aisquared/config/analytic/LocalONNXModel.py
@@ -8,20 +8,25 @@ class LocalONNXModel(BaseObject):
     """
 
     def __init__(
-        self,
-        path,
-        input_type
+            self,
+            path,
+            input_shape,
+            input_type
     ):
         """
         Parameters
         ----------
         path : str or path-like or file-like
             The file path of the saved model
+        input_shape : list
+            List of integers corresponding to the input dimensions of the model,
+            ignoring the number of samples as the first dimension
         input_type : str
             Input type to the model. Must be one of 'cv', 'text', or 'tabular'
         """
         super().__init__()
         self.path = path
+        self.input_shape = input_shape
         self.input_type = input_type
 
     @property
@@ -31,6 +36,16 @@ class LocalONNXModel(BaseObject):
     @path.setter
     def path(self, value):
         self._path = value
+
+    @property
+    def input_shape(self):
+        return self._input_shape
+
+    @input_shape.setter
+    def input_shape(self, value):
+        if not isinstance(value, list) or not all([isinstance(val, int) for val in value]):
+            raise TypeError('input_shape must be list of integers')
+        self._input_shape = value
 
     @property
     def input_type(self):
@@ -48,6 +63,7 @@ class LocalONNXModel(BaseObject):
             'className': 'LocalONNXModel',
             'params': {
                 'path': self.path,
+                'inputShape': self.input_shape,
                 'inputType': self.input_type
             }
         }
