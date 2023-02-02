ede124a76a2ce06fa9eb9ba09289c8a2478ae780
Jacob Renn <jacob.renn@squared.ai>
2023-02-02 09:00:35 -0500

    updating pie chart rendering schema
    Signed-off-by: Jacob Renn <jacob.renn@squared.ai>

--- aisquared/config/rendering/PieChartRendering.py
+++ aisquared/config/rendering/PieChartRendering.py
@@ -5,40 +5,6 @@ class PieChartRendering(BaseObject):
     """
     Rendering class for rendering a Pie Chart
 
-    Example usage:
-
-    >>> import aisquared
-    >>> my_obj = aisquared.config.rendering.PieChartRendering(
-        'my pie chart',
-        'MyPieChartID',
-        'MyPieChart',
-        ['red', 'blue'],
-        ['label1', 'label2'],
-        'my_countainer_id',
-        'key',
-        'value',
-        'name',
-        True,
-        'circle'
-    )
-    >>> my_obj.to_dict()
-    {'className': 'PieChartRendering',
-    'label': 'my pie chart',
-    'params': {'id': 'MyPieChartID',
-    'chartName': 'MyPieChart',
-    'chartColors': ['red', 'blue'],
-    'chartLabels': ['label1', 'label2'],
-    'containerId': 'my_countainer_id',
-    'predictionNameKey': 'key',
-    'predictionValueKey': 'value',
-    'predictionNameValue': 'name',
-    'displayLegend': True,
-    'legendIcon': 'circle',
-    'width': 'auto',
-    'height': 'auto',
-    'xOffset': '0',
-    'yOffset': '0'}}
-
     """
 
     def __init__(
@@ -46,18 +12,19 @@ class PieChartRendering(BaseObject):
         label: str,
         id: str,
         chart_name: str,
-        chart_colors: list,
-        chart_labels: list,
         container_id: str,
         prediction_name_key: str,
         prediction_value_key: str,
         prediction_name_value: str,
         display_legend: bool,
         legend_icon: str,
+        labels_key: str = None,
         width: str = 'auto',
         height: str = 'auto',
         xOffset: str = '0',
         yOffset: str = '0',
+        labels: list = None,
+        consolidate_rows: bool = True
 
     ):
         """
@@ -69,10 +36,6 @@ class PieChartRendering(BaseObject):
             The ID for the chart
         chart_name : str
             The name for the chart
-        chart_colors : list of str
-            List of colors to use
-        chart_labels : list of str
-            List of labels to use
         container_id : str
             The ID of the container to use
         prediction_name_key : str
@@ -85,6 +48,8 @@ class PieChartRendering(BaseObject):
             Whether to display the chart legend
         legend_icon : str
             The legend icon to display
+        labels_key : str
+            The key to use for the labels
         width : str (default 'auto')
             The width of the chart
         height : str (default 'auto')
@@ -93,13 +58,17 @@ class PieChartRendering(BaseObject):
             The offset on the x axis
         yOffset : str (default '0')
             The offset on the y axis
+        labels : list of str or None (default None)
+            Labels, if hard-coded
+        consolidate_rows : bool (default True)
+            Whether to consolidate rows in the data
+
         """
         super().__init__()
         self.label = label
         self.id = id
         self.chart_name = chart_name
-        self.chart_colors = chart_colors
-        self.chart_labels = chart_labels
+        self.labels = labels
         self.container_id = container_id
         self.prediction_name_key = prediction_name_key
         self.prediction_value_key = prediction_value_key
@@ -110,6 +79,8 @@ class PieChartRendering(BaseObject):
         self.height = height
         self.xOffset = xOffset
         self.yOffset = yOffset
+        self.consolidate_rows = consolidate_rows
+        self.labels_key = labels_key
 
     def to_dict(self) -> dict:
         """
@@ -121,17 +92,22 @@ class PieChartRendering(BaseObject):
             'params': {
                 'id': self.id,
                 'chartName': self.chart_name,
-                'chartColors': self.chart_colors,
-                'chartLabels': self.chart_labels,
                 'containerId': self.container_id,
-                'predictionNameKey': self.prediction_name_key,
-                'predictionValueKey': self.prediction_value_key,
-                'predictionNameValue': self.prediction_name_value,
                 'displayLegend': self.display_legend,
                 'legendIcon': self.legend_icon,
                 'width': self.width,
                 'height': self.height,
                 'xOffset': self.xOffset,
-                'yOffset': self.yOffset
+                'yOffset': self.yOffset,
+                'datasource': [
+                    {
+                        'labels': self.labels,
+                        'labelsKey': self.labels_key,
+                        'consolidateRows': self.consolidate_rows,
+                        'predictionNameKey': self.prediction_name_key,
+                        'predictionValueKey': self.prediction_value_key,
+                        'predictionNameValue': self.prediction_name_value
+                    }
+                ]
             }
         }
