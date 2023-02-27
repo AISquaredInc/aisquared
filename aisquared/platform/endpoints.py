550c442f835fd74b9d35e83c8e8736a4fe35c7ef
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2023-02-27 08:35:58 -0500

    finalizing appearance of endpoints file
    Signed-off-by: Jacob Renn <77127228+jacobrenn@users.noreply.github.com>

--- aisquared/platform/endpoints.py
+++ aisquared/platform/endpoints.py
@@ -1,23 +1,14 @@
 endpoints = {
-    'login': 'api/v1/auth/login',                     # For login
-    'upload_model': 'upload/v1/models',               # For uploading model
-    'model': 'api/v1/models',                         # For getting, deleting,
-    # sharing, or listing
-    # model users
-
-    'feedback': 'api/v1/feedback',                    # For model and
-    # prediction feedback
-    'user': 'userservice/v1/user',                    # For creating, updating,
-    # deleting, or
-    # getting a user
-    'group': 'groupservice/v1/group',                 # For creating, deleting,
-    # or updating groups
-
-    'group_membership': 'groupservice/v1/membership',  # For group user
-    # membership operations
-    'user_list': 'scim/v2/Users',                     # For listing users
-    'group_list': 'scim/v2/Groups',                   # For listing groups
-    'roles': 'groupservice/v1/role',                  # For listing roles
-    'usage_metrics': 'api/v1/usage-metrics',          # For usage metrics
-    'health': 'api/v1/health'                         # For health check
+    'login': 'api/v1/auth/login',
+    'upload_model': 'upload/v1/models',
+    'model': 'api/v1/models',
+    'feedback': 'api/v1/feedback',
+    'user': 'userservice/v1/user',
+    'group': 'groupservice/v1/group',
+    'group_membership': 'groupservice/v1/membership',
+    'user_list': 'scim/v2/Users',
+    'group_list': 'scim/v2/Groups',
+    'roles': 'groupservice/v1/role',
+    'usage_metrics': 'api/v1/usage-metrics',
+    'health': 'api/v1/health'
 }
