705066474a6fc194abe7c5ed6d23b1cabf8ea6e0
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2023-02-27 08:35:30 -0500

    removing comments
    Signed-off-by: Jacob Renn <77127228+jacobrenn@users.noreply.github.com>

--- aisquared/platform/endpoints.py
+++ aisquared/platform/endpoints.py
@@ -1,16 +1,20 @@
 endpoints = {
     'login': 'api/v1/auth/login',                     # For login
     'upload_model': 'upload/v1/models',               # For uploading model
-    # For getting, deleting, sharing, or listing model users
-    'model': 'api/v1/models',
-    # For model and prediction feedback
-    'feedback': 'api/v1/feedback',
-    # For creating, updating, deleting, or getting a user
-    'user': 'userservice/v1/user',
-    # For creating, deleting, or updating groups
-    'group': 'groupservice/v1/group',
-    # For group user membership operations
-    'group_membership': 'groupservice/v1/membership',
+    'model': 'api/v1/models',                         # For getting, deleting,
+    # sharing, or listing
+    # model users
+
+    'feedback': 'api/v1/feedback',                    # For model and
+    # prediction feedback
+    'user': 'userservice/v1/user',                    # For creating, updating,
+    # deleting, or
+    # getting a user
+    'group': 'groupservice/v1/group',                 # For creating, deleting,
+    # or updating groups
+
+    'group_membership': 'groupservice/v1/membership',  # For group user
+    # membership operations
     'user_list': 'scim/v2/Users',                     # For listing users
     'group_list': 'scim/v2/Groups',                   # For listing groups
     'roles': 'groupservice/v1/role',                  # For listing roles
