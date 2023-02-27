8782d4cf2ad70047f674288aa11e2fd213ca4990
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2023-02-26 19:18:07 -0500

    adding new file for user and group management functions
    Signed-off-by: Jacob Renn <77127228+jacobrenn@users.noreply.github.com>

---
+++ aisquared/platform/user_group.py
@@ -0,0 +1,44 @@
+from .AISquaredAPIException import AISquaredAPIException
+from .NoResultsFoundError import NoResultsFoundError
+from .endpoints import endpoints
+import pandas as pd
+import requests
+
+def _create_user():
+    pass
+
+def _update_user():
+    pass
+
+def _delete_user():
+    pass
+
+def _get_user():
+    pass
+
+def _get_group():
+    pass
+
+def _create_group():
+    pass
+
+def _delete_group():
+    pass
+
+def _update_group():
+    pass
+
+def _users_to_group():
+    pass
+
+def _list_users():
+    pass
+
+def _list_groups():
+    pass
+
+def _list_group_users():
+    pass
+
+def _list_roles():
+    pass
