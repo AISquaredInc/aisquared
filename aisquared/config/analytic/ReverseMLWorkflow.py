085bf206782887070f6765da5c02f489c599f8c5
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2023-12-12 11:53:16 -0500

    updating ReverseMLWorkflow config class
    Signed-off-by: Jacob Renn <77127228+jacobrenn@users.noreply.github.com>

--- aisquared/config/analytic/ReverseMLWorkflow.py
+++ aisquared/config/analytic/ReverseMLWorkflow.py
@@ -3,177 +3,86 @@ from aisquared.base import BaseObject
 
 class ReverseMLWorkflow(BaseObject):
     """
-    Interaction with a ReverseML CSV stored in S3
-
-    Example usage:
-
-    >>> import aisquared
-    >>> analytic = aisquared.config.analytic.ReverseMLWorkflow(
-        'bucket_name',
-        'file_name',
-        'column_name',
-        'text'
-    )
-    >>> analytic.to_dict()
-    {'className': 'ReverseMLWorkflow',
-    'params': {'bucket': 'bucket_name',
-    'fileNames': ['file_name'],
-    'inputType': 'text',
-    'column': 'column_name',
-    'period': None,
-    'secret': ''}}
-
+    Creation of a ReverseML Workflow to interact with remote data sources
     """
 
     def __init__(
         self,
-        bucket: str,
-        filenames: list,
-        column: str,
-        input_type: str,
-        filter_type: str,
-        filter_by_columns: list = None,
+        label: str,
+        connector_type: str,
+        connector_action: str = 'read',
+        input_type: str = 'text',
+        filter_type: str = 'input',
+        file_names: list = [],
+        bucket: str = '',
+        filter_by_columns: list = [],
+        all: bool = False,
+        arn: str = '',
+        host: str = '',
+        path: str = '',
+        port: str = '',
+        role: str = '',
+        soql: str = '',
+        query: str = '',
+        token: str = '',
+        column: str = '',
+        db_name: str = '',
+        db_user: str = '',
         period: int = None,
-        secret: str = ''
+        schema: str = '',
+        secret: str = '',
+        account: str = '',
+        data_map: list = [],
+        db_table: str = '',
+        client_id: str = '',
+        file_name: str = '',
+        password: str = '',
+        schedule: str = '',
+        sync_keys: dict = {'source': '', 'destination': ''},
+        warehouse: str = '',
+        data_source: str = '',
+        cluster_name: str = '',
+        client_secret: str = '',
+        organization: str = '',
+        authentication_type: str = ''
     ):
-        """
-        Parameters
-        ----------
-        bucket : str
-            The bucket the file lives in
-        filenames : list of str
-            The names for any specific files
-        column : str
-            The column name in the CSV file
-        input_type : str
-            Either one of 'text' or 'cv'
-        filter_type : str
-            The filter type
-        filter_by_columns : str
-            How to filter the columns of the ReverseMLWorkflow
-        period : None or int (default None)
-            The period for this to run
-        secret : str (default '')
-            A secret, if needed
-        """
-        super().__init__()
-        self.bucket = bucket
-        self.filenames = filenames
-        self.column = column
+        self.label = label
+        self.connector_type = connector_type
+        self.connector_action = connector_action
         self.input_type = input_type
         self.filter_type = filter_type
+        self.file_names = file_names
+        self.bucket = bucket
         self.filter_by_columns = filter_by_columns
+        self.all = all
+        self.arn = arn
+        self.host = host
+        self.path = path
+        self.port = port
+        self.role = role
+        self.soql = soql
+        self.query = query
+        self.token = token
+        self.column = column
+        self.db_name = db_name
+        self.db_user = db_user
         self.period = period
+        self.schema = schema
         self.secret = secret
-
-    @property
-    def bucket(self):
-        return self._bucket
-
-    @bucket.setter
-    def bucket(self, value):
-        if not isinstance(value, str):
-            raise TypeError('bucket must be string')
-        self._bucket = value
-
-    @property
-    def filenames(self):
-        return self._filenames
-
-    @filenames.setter
-    def filenames(self, value):
-        if isinstance(value, str):
-            value = [value]
-        if not isinstance(value, list):
-            raise TypeError('filenames must be a list')
-        if not all([isinstance(v, str) for v in value]):
-            raise TypeError('filenames must all be strings')
-        self._filenames = value
-
-    @property
-    def column(self):
-        return self._column
-
-    @column.setter
-    def column(self, value):
-        if not isinstance(value, str):
-            raise TypeError('column must be a string')
-        self._column = value
-
-    @property
-    def input_type(self):
-        return self._input_type
-
-    @input_type.setter
-    def input_type(self, value):
-        self._input_type = value
-
-    @property
-    def filter_type(self):
-        return self._filter_type
-
-    @filter_type.setter
-    def filter_type(self, value):
-        if not isinstance(value, str):
-            raise TypeError('filter_type must be str')
-        self._filter_type = value
-
-    @property
-    def filter_by_columns(self):
-        return self._filter_by_columns
-
-    @filter_by_columns.setter
-    def filter_by_columns(self, value):
-        if not isinstance(value, list):
-            raise TypeError('filter_by_columns must be list')
-        if not all([isinstance(v, dict) for v in value]):
-            raise TypeError('All items in filter_by_columns must be dict')
-
-        def _check_item(v):
-            if 'inputType' not in v.keys():
-                raise ValueError(
-                    'All items in filter_by_columns must have "inputType" key')
-
-            if v['inputType'] not in v.keys():
-                if 'columnValue' not in v.keys():
-                    raise ValueError(
-                        'All items in filter_by_columns must have "columnValue" key if "inputType" is "static"')
-
-            if 'columnName' not in v.keys():
-                raise ValueError(
-                    'All items in filter_by_columns must have "columnName" ke')
-
-            if v['inputType'] not in ['group', 'input', 'static']:
-                raise ValueError(
-                    f'"inputType" must be one of "group", "input", or "static", got {v["inputType"]}')
-
-            return True
-
-        if not all([_check_item(v) for v in value]):
-            raise ValueError('Not all items pass validation')
-
-        self._filter_by_columns = value
-
-    @property
-    def period(self):
-        return self._period
-
-    @period.setter
-    def period(self, value):
-        if value is not None:
-            if not isinstance(value, int):
-                raise TypeError('period must be int if provided')
-            if value < 1:
-                raise ValueError('period must be greater than or equal to 1')
-        self._period = value
-
-    @property
-    def secret(self):
-        return self._secret
-
-    @secret.setter
-    def secret(self, value):
-        self._secret = value
+        self.account = account
+        self.data_map = data_map
+        self.db_table = db_table
+        self.client_id = client_id
+        self.file_name = file_name
+        self.password = password
+        self.schedule = schedule
+        self.sync_keys = sync_keys
+        self.warehouse = warehouse
+        self.data_source = data_source
+        self.cluster_name = cluster_name
+        self.client_secret = client_secret
+        self.organization = organization
+        self.authentication_type = authentication_type
 
     def to_dict(self) -> dict:
         """
@@ -181,14 +90,43 @@ class ReverseMLWorkflow(BaseObject):
         """
         return {
             'className': 'ReverseMLWorkflow',
+            'label': self.label,
             'params': {
-                'bucket': self.bucket,
-                'fileNames': self.filenames,
+                'connectorType': self.connector_type,
+                'connectorAction': self.connector_action,
                 'inputType': self.input_type,
                 'filterType': self.filter_type,
+                'fileNames': self.file_names,
+                'bucket': self.bucket,
+                'filterByColumns': self.filter_by_columns,
+                'all': self.all,
+                'arn': self.arn,
+                'host': self.host,
+                'path': self.path,
+                'port': self.port,
+                'role': self.role,
+                'soql': self.soql,
+                'query': self.query,
+                'token': self.token,
                 'column': self.column,
+                'dbName': self.db_name,
+                'dbUser': self.db_user,
                 'period': self.period,
+                'schema': self.schema,
                 'secret': self.secret,
-                'filterByColumns': self.filter_by_columns
+                'account': self.account,
+                'dataMap': self.data_map,
+                'dbTable': self.db_table,
+                'clientId': self.client_id,
+                'fileName': self.file_name,
+                'password': self.password,
+                'schedule': self.schedule,
+                'syncKeys': self.sync_keys,
+                'warehouse': self.warehouse,
+                'dataSource': self.data_source,
+                'clusterName': self.cluster_name,
+                'clientSecret': self.client_secret,
+                'organization': self.organization,
+                'authenticationType': self.authentication_type
             }
         }
